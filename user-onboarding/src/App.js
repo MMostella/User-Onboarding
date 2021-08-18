fd85bad533d33433e47d1cb124d573473d34e7b9
MMostella <masonmostella@gmail.com>
2021-08-18 17:13:02 -0500

    Set up axios requests

--- user-onboarding/src/App.js
+++ user-onboarding/src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import Form from './components/Form';
 
 import axios from 'axios';
@@ -7,7 +7,42 @@ import schema from './validation/formSchema';
 
 import './App.css';
 
+const initialFormValues = {
+  name: '',
+  email: '',
+  password: '',
+  term: false,
+}
+const initialFormErrors = {
+  name: '',
+  email: '',
+  password: '',
+}
+const initialUsers = []
+const initialDisabled = true
+
 function App() {
+  const [users, setUsers] = useState(initialUsers)
+  const [formValues, setFormValues] = useState(initialFormValues)
+  const [formErrors, setFormErrors] = useState(initialFormErrors)
+  const [disabled, setDisabled] = useState(initialDisabled)
+
+  const getUser = () => {
+    axios.get('https://reqres.in/api/users')
+      .then(res => {
+        console.log(res);
+      })
+      .catch(err => console.error(err))
+  }
+
+  const postNewUser = newUser => {
+    axios.post('https://reqres.in/api/users', newUser)
+      .then(res => {
+        // console.log(res);
+      })
+      .catch(err => console.error(err))
+  }
+
   return (
     <div className="App">
       <h1>Its Working</h1>
